e2ba3b7a1cd1c01e69b342fc690ae2103025d94d
Ben Slater <ben@overmation.com>
2015-05-29 13:29:51 +0100

    add length of presence validation to tasklist title

--- app/models/tasklist.rb
+++ app/models/tasklist.rb
@@ -1,4 +1,6 @@
 class Tasklist < ActiveRecord::Base
 	validates :title, presence: true
 	validates :title, length: { minimum: 3 }
+	validates :description, presence: true
+	validates :description, length: { minimum: 3 }
 end
