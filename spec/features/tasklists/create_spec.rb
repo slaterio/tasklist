4d39dbaa0024a499f5b6dd529f8d281cd4a83e83
Ben Slater <ben@overmation.com>
2015-05-29 13:30:45 +0100

    add length of presence validation to tasklist title

--- spec/features/tasklists/create_spec.rb
+++ spec/features/tasklists/create_spec.rb
@@ -49,4 +49,40 @@ it "displays an error when the tasklist has a title less than three characters" 
 	expect(page).to_not have_content("This is what I'm doing today")
 	end
 
+it "displays an error when the tasklist has no description" do
+	expect(Tasklist.count).to eq(0)
+
+	visit "/tasklists"
+	click_link "New Tasklist"
+	expect(page).to have_content("New Tasklist")
+
+	fill_in "Title", with: "My Tasklist"
+	fill_in "Description", with: ""
+	click_button "Create Tasklist"
+
+	expect(page).to have_content("error")
+	expect(Tasklist.count).to eq(0)
+
+	visit "/tasklists"
+	expect(page).to_not have_content("This is what I'm doing today")
+	end
+
+it "displays an error when the tasklist has a description less than three characters" do
+	expect(Tasklist.count).to eq(0)
+
+	visit "/tasklists"
+	click_link "New Tasklist"
+	expect(page).to have_content("New Tasklist")
+
+	fill_in "Title", with: "My Tasklist"
+	fill_in "Description", with: "No"
+	click_button "Create Tasklist"
+
+	expect(page).to have_content("error")
+	expect(Tasklist.count).to eq(0)
+
+	visit "/tasklists"
+	expect(page).to_not have_content("This is what I'm doing today")
+	end
+
 end
