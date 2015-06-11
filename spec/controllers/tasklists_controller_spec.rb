286a6809882248ef8a9a2bb2ef1e56bfa32de403
Ben Slater <ben@overmation.com>
2015-06-11 17:04:10 +0100

    add specs for deleting tasklists

--- spec/controllers/tasklists_controller_spec.rb
+++ spec/controllers/tasklists_controller_spec.rb
@@ -23,7 +23,7 @@ describe TasklistsController do
   # This should return the minimal set of attributes required to create a valid
   # Tasklist. As you add validations to Tasklist, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "title" => "MyString" } }
+  let(:valid_attributes) { { "title" => "MyString", "description" => "My description" } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
---
+++ spec/features/tasklists/destroy_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "deleting tasklists" do 
+
+	let!(:tasklist) { Tasklist.create(title: "Groceries", description: "Grocery list.") }
+
+
+	it "is successful when clicking destroy link" do
+
+	visit "/tasklists"
+	within "#tasklist_#{tasklist.id}" do
+		click_link "Destroy"
+		end
+
+		expect(page).to_not have_content(tasklist.title)
+		expect(Tasklist.count).to eq(0)
+
+	end
+
+
+end
